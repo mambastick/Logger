21f023cfa0fc310f6e2897f13a860d296599e1ac
Neelov Eugene <neelov.ev@mail.ru>
2023-10-17 01:21:57 +0300

    Fix with async write logs (file busy)

--- Logger/Logger.cs
+++ Logger/Logger.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace LoggerService;
 
@@ -16,6 +19,9 @@ public enum Level
 public class Logger
 {
     private readonly string LogFilePath;
+    private readonly Queue<string> logQueue = new Queue<string>();
+    private readonly object logLock = new object();
+    private bool isWriting = false;
 
     public Logger()
     {
@@ -24,36 +30,35 @@ public class Logger
 
         var logFileName = $"{DateTime.Now:dd-MM-yyyy_HH-mm-ss}.log";
         LogFilePath = Path.Combine(logFolder, logFileName);
+
+        Task.Factory.StartNew(ProcessLogQueue, TaskCreationOptions.LongRunning);
     }
 
     private void Log(Level level, string message, [CallerMemberName] string caller = null)
     {
         var timestamp = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
-        
+
         if (caller == ".ctor")
-            caller = $"{new System.Diagnostics.StackTrace(1).GetFrame(1)?.GetMethod()?.ReflectedType?.Name} constructor";
-        
+            caller =
+                $"{new System.Diagnostics.StackTrace(1).GetFrame(1)?.GetMethod()?.ReflectedType?.Name} constructor";
+
         var callerInfo = string.IsNullOrWhiteSpace(caller) ? "" : $"[{caller}]";
-        
+
         var messageLines = message.Split('\n');
-    
+
         foreach (var line in messageLines)
         {
-            // ReSharper disable once HeapView.BoxingAllocation
             var logEntry = $"[{timestamp}] [{level.ToString().ToUpper()}] {callerInfo} {line}";
 
             Console.ForegroundColor = GetConsoleColor(level);
             Console.WriteLine(logEntry);
             Console.ResetColor();
 
-            LogToFile(logEntry);
+            lock (logLock)
+            {
+                logQueue.Enqueue(logEntry);
+            }
         }
-    }
-
-    private void LogToFile(string logEntry)
-    {
-        using var writer = File.AppendText(LogFilePath);
-        writer.WriteLine(logEntry);
     }
 
     private static ConsoleColor GetConsoleColor(Level level) => level switch
@@ -67,14 +72,56 @@ public class Logger
         Level.Fatal => ConsoleColor.DarkRed,
         _ => ConsoleColor.Gray
     };
+    
+    private void ProcessLogQueue()
+    {
+        while (true)
+        {
+            string logEntry = null;
+
+            lock (logLock)
+                if (logQueue.Count > 0)
+                    logEntry = logQueue.Dequeue();
+
+            if (logEntry != null)
+                WriteLogToFile(logEntry);
+            else
+                Thread.Sleep(100);
+        }
+    }
+
+    private async void WriteLogToFile(string logEntry)
+    {
+        while (isWriting)
+            await Task.Delay(10);
+
+        isWriting = true;
+        try
+        {
+            await using var writer = File.AppendText(LogFilePath);
+            await writer.WriteLineAsync(logEntry);
+        }
+        finally
+        {
+            isWriting = false;
+        }
+    }
 
     public string GetLogFilePath() => LogFilePath;
-    
     public void LogDebug(string message, [CallerMemberName] string caller = null) => Log(Level.Debug, message, caller);
-    public void LogInformation(string message, [CallerMemberName] string caller = null) => Log(Level.Info, message, caller);
-    public void LogProcess(string message, [CallerMemberName] string caller = null) => Log(Level.Process, message, caller);
-    public void LogSuccess(string message, [CallerMemberName] string caller = null) => Log(Level.Success, message, caller);
-    public void LogWarning(string message, [CallerMemberName] string caller = null) => Log(Level.Warning, message, caller);
+
+    public void LogInformation(string message, [CallerMemberName] string caller = null) =>
+        Log(Level.Info, message, caller);
+
+    public void LogProcess(string message, [CallerMemberName] string caller = null) =>
+        Log(Level.Process, message, caller);
+
+    public void LogSuccess(string message, [CallerMemberName] string caller = null) =>
+        Log(Level.Success, message, caller);
+
+    public void LogWarning(string message, [CallerMemberName] string caller = null) =>
+        Log(Level.Warning, message, caller);
+
     public void LogError(string message, [CallerMemberName] string caller = null) => Log(Level.Error, message, caller);
     public void LogFatal(string message, [CallerMemberName] string caller = null) => Log(Level.Fatal, message, caller);
 }
